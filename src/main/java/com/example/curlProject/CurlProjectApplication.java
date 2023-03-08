89597486cc6ba79105f8c36901e69d6d471a9ccd
haryang Park <haryang92@gmail.com>
2023-03-08 17:41:40 +0900

    Initial Commit

---
+++ src/main/java/com/example/curlProject/CurlProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.curlProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurlProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurlProjectApplication.class, args);
+	}
+
+}
